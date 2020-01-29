3d194c1757a5334ba0b1ede25c845a6fa59a3a6a
Anmol <anmolsingh1597@gmail.com>
2020-01-28 21:06:53 -0500

    [Anmol Singh] ArrayList

--- src/main/java/com/lambton/MyMainClass.java
+++ src/main/java/com/lambton/MyMainClass.java
@@ -1,9 +1,14 @@
 package com.lambton;
 
+//import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class MyMainClass {
 
     public static void main(String[] args) {
-        // Hi Aman!!
+        ArrayList<String>countryList=new ArrayList<>();
+        countryList.add("Canada");
+        countryList.add("India");
     }
 
 }
